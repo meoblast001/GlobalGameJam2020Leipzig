ecc058e28308e175e80a744c22b1ce6406547a80
Sven Laschinski <sven@laschinski.net>
2020-02-02 00:27:49 +0100

    add working VR controles

---
+++ Assets/Scripts/VRController/WinMRController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+
+public class WinMRController : MonoBehaviour
+{
+    public enum LeftRight
+    {
+        Left, Right
+    }
+
+    private InputMaster controls;
+    [SerializeField] private LeftRight leftRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateControllerPosition();
+    }
+
+    void Awake()
+    {
+        this.controls = new InputMaster();
+        Application.onBeforeRender += this.updateControllerPosition;
+    }
+    void OnEnable()
+    {
+        this.controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        this.controls.Disable();
+    }
+
+    void updateControllerPosition()
+    {
+        switch (leftRight)
+        {
+            case LeftRight.Left:
+                this.transform.localRotation = this.controls.VRPlayer.LeftRotation.ReadValue<Quaternion>();
+                this.transform.localPosition = this.controls.VRPlayer.LeftPosition.ReadValue<Vector3>();
+                break;
+            case LeftRight.Right:
+                this.transform.localRotation = this.controls.VRPlayer.RightRotation.ReadValue<Quaternion>();
+                this.transform.localPosition = this.controls.VRPlayer.RightPosition.ReadValue<Vector3>();
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/VRController/WinMRHMD.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+
+public class WinMRHMD : MonoBehaviour
+{
+    private InputMaster controls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHMDPosition();
+    }
+
+    void Awake()
+    {
+        this.controls = new InputMaster();
+        //this.controls.VRPlayer.VRHead.started += ctx => this.DoSomething(ctx.ReadValue<Quaternion>());
+        Application.onBeforeRender += this.updateHMDPosition;
+    }
+
+    void OnEnable()
+    {
+        this.controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        this.controls.Disable();
+    }
+
+    void updateHMDPosition()
+    {
+        var rotationHMD = InputSystem.GetDevice<XRHMD>()?.deviceRotation?.ReadValue();
+        if (rotationHMD.HasValue)
+        {
+            this.transform.localRotation = rotationHMD.Value;
+        }
+
+        var positionHMD = InputSystem.GetDevice<XRHMD>()?.devicePosition?.ReadValue();
+        if (positionHMD.HasValue)
+        {
+            this.transform.localPosition = positionHMD.Value;
+        }
+    }
+}
