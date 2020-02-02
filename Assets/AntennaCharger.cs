b239df293882031933f426cee93be90449e110e1
Sven Laschinski <sven@laschinski.net>
2020-02-02 15:40:58 +0100

    add basic VR interaction

---
+++ Assets/AntennaCharger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class AntennaCharger : MonoBehaviour
+{
+    private Collider lastCollider;
+    [Inject] private HealthStatus healthStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9 && other != lastCollider)
+        {
+            lastCollider = other;
+            healthStatus.Shield += 1;
+        }
+    }
+
+}
