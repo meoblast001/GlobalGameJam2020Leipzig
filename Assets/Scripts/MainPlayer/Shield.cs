08c5c4409897419f52e05101c7b78cf96e087780
Braden Walters <vc@braden-walters.info>
2020-02-02 16:50:33 +0100

    Shield eats health; Attempt at collision

--- Assets/Scripts/MainPlayer/Shield.cs
+++ Assets/Scripts/MainPlayer/Shield.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using Zenject;
 
 public class Shield : MonoBehaviour
 {
+    public int usageLoseHealth = 5;
+    public float usageLoseHealthEverySeconds = 0.5f;
+ 
     private InputMaster controls;
 
+    [Inject] private HealthStatus healthStatus;
+
     private Vector2 movement;
+    private float usageSeconds;
 
     void Awake()
     {
@@ -15,16 +22,30 @@ public class Shield : MonoBehaviour
     void Update()
     {
         this.RotateShield(this.controls.MainPlayer.RotateShield.ReadValue<Vector2>());
+        this.usageSeconds += Time.deltaTime;
+
+        while (this.usageSeconds >= this.usageLoseHealthEverySeconds)
+        {
+            this.healthStatus.Shield -= this.usageLoseHealth;
+            this.usageSeconds -= this.usageLoseHealthEverySeconds;
+        }
     }
 
     void OnEnable()
     {
+        this.usageSeconds = this.usageLoseHealthEverySeconds;
         this.controls.Enable();
     }
 
     void OnDisable()
     {
         this.controls.Disable();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rocket>() != null)
+            GameObject.Destroy(other);
     }
 
     private void RotateShield(Vector2 direction)
